83d2f94ab48d367938578102a854945110a72325
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-12-24 23:26:47 +0530

    frontend env updated with backend link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // ✅ REQUEST INTERCEPTOR (TOKEN ATTACH)
